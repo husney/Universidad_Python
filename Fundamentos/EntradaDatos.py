a4f69e64a71a2ec26b0a5183f71fc9c74027a6af
husney_bayona <husney_bayona@cnt.com.co>
2020-12-10 20:54:13 -0500

    Operadores aritmeticos

--- Fundamentos/EntradaDatos.py
+++ Fundamentos/EntradaDatos.py
@@ -7,4 +7,10 @@ print(prueba)
 
 numero = int(input("Por favor ingrse un número: "))
 
-flotante = float(input("Por favor ingruese un número decimal"))
+flotante = float(input("Por favor ingruese un número decimal: "))
+
+
+titulo = input("Proporciona el titulo: ")
+autor = input("Proporciona el autor")
+
+print(titulo + " fue escrito por " + autor)
---
+++ Operadores/aritmeticos.py
@@ -0,0 +1,18 @@
+a = 3
+b = 2
+suma = a + b
+print("suma: " , suma)
+resta = a - b
+print("resta: " , resta)
+multiplicacion = a * b
+print("multiplicación: " , multiplicacion)
+division = a / b
+print("división: " , division)
+divisionEntera = a // b
+print("division entera : " , divisionEntera)
+modulo = a % b
+print("modulo: " , modulo)
+potencia = a ** b
+print("potencia: " , potencia)
+
+
