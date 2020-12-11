babbec1a349ba7dbc49affdb4cb88a6f050d7f5e
husney_bayona <husney_bayona@cnt.com.co>
2020-12-10 20:22:38 -0500

    Datos booleanos

---
+++ Fundamentos/DatosBool.py
@@ -0,0 +1,24 @@
+x = True
+print(type(x))
+print(x)
+
+y = False
+print(type(y))
+print(y)
+
+
+n1 = 1
+n2 = 2
+resultado  = n1 > n2
+print(resultado)
+
+
+if n1 < n2:
+    print("El primer valor es menor al segundo valor")
+else:
+    print("El segundo valor es mayor al primer valor")
+    
+    
+    
+    
+
