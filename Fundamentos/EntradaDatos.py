74a480aa405730a479fed59107992f6447ffd048
husney_bayona <husney_bayona@cnt.com.co>
2020-12-10 20:34:43 -0500

    Entrada de datos

---
+++ Fundamentos/EntradaDatos.py
@@ -0,0 +1,10 @@
+nombre = input("Por favor ingrese su nombre:  ")
+print(nombre)
+
+prueba = str(20)
+print(type(prueba))
+print(prueba)
+
+numero = int(input("Por favor ingrse un número: "))
+
+flotante = float(input("Por favor ingruese un número decimal"))
