d6c01d6f1e4ac50450ef7033b3dc58abf6376b43
husney_bayona <husney_bayona@cnt.com.co>
2020-12-10 21:31:26 -0500

    Operadores de comparacion

---
+++ Operadores/comparacion.py
@@ -0,0 +1,27 @@
+
+a = 3
+b = 2
+
+resultado = a == b
+print(resultado)
+
+resultado = a != b
+print(resultado)
+
+resultado = a > b
+print(resultado)
+
+resultado = a < b
+print(resultado)
+
+resultado = a >= b
+print(resultado)
+
+resultado = a <= b
+print(resultado)
+ 
+ 
+if a % 2 == 0:  
+    print(f"El número {a} es par")
+else:
+    print(f"El número {a} es inpar")
